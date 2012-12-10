64a366a44047d76036adaa82cafbac71cc836ae1
rtnpro <rtnpro@transifex.com>
2012-12-10 16:40:34 +0530

    More optimization and bugfixes.

--- CdrAnalytics/cdr/management/commands/max_call_count_stats.py
+++ CdrAnalytics/cdr/management/commands/max_call_count_stats.py
@@ -7,7 +7,11 @@ class Command(BaseCommand):
     help = "Index max concurrent calls count stats"
 
     def handle(self, *args, **options):
-        d1 = MaxConCallCountPerHour.objects.aggregate(max=Max('hour'))['max'] + timedelta(hours=1)
+        last_indexed_hour = MaxConCallCountPerHour.objects.aggregate(
+                max=Max('hour'))['max']
+        
+        d1 = last_indexed_hour + timedelta(hours=1) if last_indexed_hour else \
+                        datetime.strptime('9/1/2012', '%m/%d/%Y')
         d2 = datetime.strptime('10/1/2012', '%m/%d/%Y') - timedelta(hours=1)
         print "Indexing max concurrent call count stats from %s to %s" % (d1, d2)
         index_max_con_call_count_per_hour(d1, d2)
--- CdrAnalytics/cdr/models.py
+++ CdrAnalytics/cdr/models.py
@@ -3,8 +3,7 @@ from django.db import models
 from djangobulk.bulk import insert_many, update_many
 from django.utils.translation import ugettext_lazy as _
 from timedelta.fields import TimedeltaField
-from django.db.models import F
-from django.db.models import Max, Min
+from django.db.models import F, Q, Max, Min
 
 
 class TempCallDetailRecord(models.Model):
@@ -19,21 +18,34 @@ class CallDetailRecordManager(models.Manager):
                 end__gte=t, start__lt=t).count()
 
     def get_max_concurrent_calls_for_an_hour(self, h):
-        TempCallDetailRecord.objects.all().delete()
-        TempCallDetailRecord.objects.bulk_create([
-            TempCallDetailRecord(start=i['start'],
-                end=i['start'] + i['duration']
-            ) for i in self.filter(start__gte=h - timedelta(hours=1),
-                start__lt=h).values('start', 'duration').iterator()
-        ])
-        return max([self.concurrent_calls_count_at_time(t)
-            for t in set([h] + [
-                    i[0] for i in self.filter(
-                    start__gte=h, start__lt=h + timedelta(hours=1)
-                    ).distinct('start').values_list('start')
-                ]
-            )
-        ])
+        initial_count = self.filter(start__gt=h - F('duration'),
+                start__lt=h).values('id').count()
+        start_dict = {}
+        end_dict = {}
+        cur_count = max_count = initial_count
+        for i in self.filter(start__gte=h, start__lt=h + timedelta(hours=1)
+                ).values('start').iterator():
+            start = i['start']
+            if start not in start_dict:
+                start_dict[start] = 1
+            else:
+                start_dict[start] += 1
+
+        for i in self.filter(start__gte=h - F('duration'),
+                 start__lt=h + timedelta(hours=1) - F('duration')
+                 ).values('start', 'duration').iterator():
+            end = i['start'] + i['duration']
+            if end not in end_dict:
+                end_dict[end] = 1
+            else:
+                end_dict[end] += 1
+
+        for i in range(3600):
+            t = h + timedelta(seconds=i)
+            cur_count += start_dict.get(t, 0) - end_dict.get(t, 0)
+            if cur_count > max_count:
+                max_count = cur_count
+        return max_count
 
 
 class CallDetailRecord(models.Model):
@@ -87,12 +99,14 @@ def index_max_con_call_count_per_hour(lb=None, ub=None):
                 )['ub']
              )
     while lb <= ub:
+        d = datetime.now()
         max_count = CallDetailRecord.objects.get_max_concurrent_calls_for_an_hour(lb)
         m, created = MaxConCallCountPerHour.objects.get_or_create(
                 hour=lb, defaults={'max_con_count':max_count})
         if not created:
             m.update(max_con_count=max_count)
-        print lb
+        print "Calculated max concurrent call count for %s in %d seconds: %d" % (
+                lb, (datetime.now() - d).seconds, max_count)
         lb += timedelta(hours=1)
 
 
