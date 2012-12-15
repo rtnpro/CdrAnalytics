1e5c24d10de42e3a629b64510eee056e696d7c9c
rtnpro <rtnpro@transifex.com>
2012-12-15 21:03:48 +0530

    Use raphy-chart to show max concurrent call analytics.

--- CdrAnalytics/cdr/views.py
+++ CdrAnalytics/cdr/views.py
@@ -4,34 +4,34 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from CdrAnalytics.cdr.forms import DateRangeForm, DateTimeRangeForm
 from CdrAnalytics.cdr.models import *
+from CdrAnalytics.raphycharts.charts import RaphyLineChart
 from datetime import datetime
 
 
 def get_plot(from_date, to_date):
-    return {
-            'plots': [
-                        (datetime.strftime(i[0], '%Y-%m-%d %I:00%p'), i[1])
-                        for i in MaxConCallCountPerHour.objects.filter(
-                            hour__gte=from_date, hour__lt=to_date
-                            ).order_by('hour').values_list
-                        ('hour', 'max_con_count')
-                     ],
-            'min_date': datetime.strftime(from_date, '%b %d, %Y'),
-            'max_date': datetime.strftime(to_date, '%b %d, %Y')
-    }
+    return RaphyLineChart(
+            data=[
+                (i[0], i[1])
+                for i in MaxConCallCountPerHour.objects.filter(
+                    hour__gte=from_date, hour__lt=to_date
+                    ).order_by('hour').values_list
+                ('hour', 'max_con_count')
+            ], x_axis_type="date"
+    )
 
 
 def max_con_call_analytics(request):
+    plot = None
     if request.method == "POST":
         form = DateRangeForm(request.POST)
         if form.is_valid():
             plot = get_plot(form.cleaned_data['from_date'],
                     form.cleaned_data['to_date'])
-            return HttpResponse(json.dumps(plot), mimetype="application/json")
     else:
         form = DateRangeForm()
     return render_to_response('cdr/max_con_call_analytics.html',
-            {'form': form}, context_instance=RequestContext(request))
+            {'form': form, 'plot': plot},
+            context_instance=RequestContext(request))
 
 
 def home(request):
